90a7cfa70f12f8a613e6ac8d36c28fc65ecbf00b
chenyshi <chenyshi@yonyou.com>
2022-06-15 18:24:26 +0800

    telerik本地化

--- TelerikDemo/Program.cs
+++ TelerikDemo/Program.cs
@@ -1,6 +1,11 @@
 using CommonLib;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+using Telerik.Blazor.Services;
+using TelerikDemo;
 using TelerikDemo.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,9 +16,28 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddTelerikBlazor();
 builder.Services.AddCommonLibService();
+builder.Services.AddSingleton(typeof(ITelerikStringLocalizer), typeof(SampleResxLocalizer));
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    // define the list of cultures your app will support
+    var supportedCultures = new List<CultureInfo>()
+            {
+                new CultureInfo("zh-CN"),
+                new CultureInfo("de-DE"),
+                new CultureInfo("es-ES"),
+                new CultureInfo("bg-BG")
+            };
 
+    // set the default culture
+    options.DefaultRequestCulture = new RequestCulture("zh-CN");
+
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
 var app = builder.Build();
-
+//app.UseRequestLocalization(app.Services.GetService<IOptions<RequestLocalizationOptions>>().Value);
+app.UseRequestLocalization("zh-CN");
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
---
+++ TelerikDemo/SampleResxLocalizer.cs
@@ -0,0 +1,24 @@
+﻿using Telerik.Blazor.Services;
+using TelerikDemo.Resources;
+
+namespace TelerikDemo
+{
+    
+    public class SampleResxLocalizer : ITelerikStringLocalizer
+    {
+        // this is the indexer you must implement
+        public string this[string name]
+        {
+            get
+            {
+                return GetStringFromResource(name);
+            }
+        }
+
+        // sample implementation - uses .resx files in the ~/Resources folder named TelerikMessages.<culture-locale>.resx
+        public string GetStringFromResource(string key)
+        {
+            return TelerikMessages.ResourceManager.GetString(key, Resources.TelerikMessages.Culture); ;
+        }
+    }
+}
